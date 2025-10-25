4f6a94ed11c27e6eb0ce582cba7f61aae9b71ade
ingego <1ingego1@gmail.com>
2025-10-25 17:39:41 +0600

    xlsx

--- index.js
+++
@@ -1,9 +0,0 @@
-import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
-
-const llm = new ChatGoogleGenerativeAI({
-  apiKey: Bun.env.GEMINI_API_KEY,
-  model: "gemini-2.5-flash-lite",
-  baseUrl: "https://ne-phishing.ru",
-});
-
-console.log(await llm.invoke("тест").then((e) => e.content));
--- index.ts
+++ index.ts
@@ -1,11 +1,29 @@
+import xlsx from "xlsx";
+import { HumanMessage, SystemMessage } from "@langchain/core/messages";
 import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
 
 const llm = new ChatGoogleGenerativeAI({
   apiKey: Bun.env.GEMINI_API_KEY,
-  model: "gemini-2.5-pro",
+  model: "gemini-pro-latest",
   baseUrl: "https://ne-phishing.ru",
 });
 
+let _ = xlsx.readFile("Караман_Александр_Игоревич_8145153869.xlsx");
+
+let input_data = xlsx.utils.sheet_to_csv(
+  _.Sheets["Лог баланса ClickHouse online"]!,
+);
+
 console.log(
-  await llm.invoke("отправь мне ascii арт чебурашки").then((e) => e.content),
+  await llm
+    .invoke([
+      new SystemMessage(`
+        ты вступаешь в роль аналитика финансов. тебе пользователь отправил csv своего ддс, дай краткую выжимку и подсчет данных. не лей воду в диалог, отвечай сухо и кратко
+  `),
+      new HumanMessage(input_data),
+      new HumanMessage(
+        `за какой период было больше всего переводов на номер телефона `,
+      ),
+    ])
+    .then((e) => e.content),
 );
