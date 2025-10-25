dbba0ea0513b26945e6c4e506605c2f2bafe6c97
ingego <1ingego1@gmail.com>
2025-10-25 17:56:34 +0600

    pdf_with_xlsx

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
+import xlsx from "xlsx";
 import { HumanMessage, SystemMessage } from "@langchain/core/messages";
 import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
 import { PDFParse } from "pdf-parse";
@@ -6,6 +7,11 @@ const llm = new ChatGoogleGenerativeAI({
   model: "gemini-pro-latest",
   baseUrl: "https://ne-phishing.ru",
 });
+let _ = xlsx.readFile("Караман_Александр_Игоревич_8145153869.xlsx");
+
+let input_data = xlsx.utils.sheet_to_csv(
+  _.Sheets["Лог баланса ClickHouse online"]!,
+);
 
 const pdf = new PDFParse({
   data: await Bun.file("Караман Александр Игоревич итог.pdf").bytes(),
@@ -17,7 +23,11 @@ console.log(
       new SystemMessage(`
         ты вступаешь в роль аналитика финансов. тебе пользователь отправил csv своего ддс, дай краткую выжимку и подсчет данных. не лей воду в диалог, отвечай сухо и кратко
   `),
-      new HumanMessage((await pdf.getText()).text),
+      new HumanMessage(
+        "это наш файл Справка с изменением доступного остатка по договору" +
+          (await pdf.getText()).text,
+      ),
+      new HumanMessage("это наш ддс" + input_data),
       new HumanMessage(`дай краткую сводку`),
     ])
     .then((e) => e.content),
