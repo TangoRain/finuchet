9f025208409dd09b883258f0acd88f4b785e8ec2
ingego <1ingego1@gmail.com>
2025-10-25 17:53:01 +0600

    add pdf

--- index.ts
+++ index.ts
@@ -1,18 +1,15 @@
-import xlsx from "xlsx";
 import { HumanMessage, SystemMessage } from "@langchain/core/messages";
 import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
-
+import { PDFParse } from "pdf-parse";
 const llm = new ChatGoogleGenerativeAI({
   apiKey: Bun.env.GEMINI_API_KEY,
   model: "gemini-pro-latest",
   baseUrl: "https://ne-phishing.ru",
 });
 
-let _ = xlsx.readFile("Караман_Александр_Игоревич_8145153869.xlsx");
-
-let input_data = xlsx.utils.sheet_to_csv(
-  _.Sheets["Лог баланса ClickHouse online"]!,
-);
+const pdf = new PDFParse({
+  data: await Bun.file("Караман Александр Игоревич итог.pdf").bytes(),
+});
 
 console.log(
   await llm
@@ -20,10 +17,8 @@ console.log(
       new SystemMessage(`
         ты вступаешь в роль аналитика финансов. тебе пользователь отправил csv своего ддс, дай краткую выжимку и подсчет данных. не лей воду в диалог, отвечай сухо и кратко
   `),
-      new HumanMessage(input_data),
-      new HumanMessage(
-        `за какой период было больше всего переводов на номер телефона `,
-      ),
+      new HumanMessage((await pdf.getText()).text),
+      new HumanMessage(`дай краткую сводку`),
     ])
     .then((e) => e.content),
 );
